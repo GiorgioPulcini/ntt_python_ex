b2048319b5327c331db742a4328c0a524d55c795
GIORGIO PULCINI <giorgio.pulcini.nttdata@skytv.it>
2024-03-23 12:06:39 +0100

    Refactor hello test

--- Matemathic_Function/test/matemathic_function_tests/hello_test.py
+++ Matemathic_Function/test/matemathic_function_tests/hello_test.py
@@ -1,11 +1,13 @@
+import unittest
 from Matemathic_Function.src.matemathic_function.hello import hello
 
 
-def test_hello():
-    # Given
-    given_str = "Hello friend"
-    given_name = "friend"
-    # When
-    test_str = hello(given_name)
-    # then
-    assert given_str == test_str
+class HelloTest(unittest.TestCase):
+    def test_hello(self):
+        # Given
+        given_str = "Hello friend"
+        given_name = "friend"
+        # When
+        test_str = hello(given_name)
+        # Then
+        self.assertEqual(given_str, test_str)
