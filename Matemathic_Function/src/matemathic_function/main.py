993cde6440238d90ef5e7c6662f52be2d72f5dc5
GIORGIO PULCINI <giorgio.pulcini.nttdata@skytv.it>
2024-03-23 13:47:35 +0100

    Add try-except to main

--- Matemathic_Function/src/matemathic_function/main.py
+++ Matemathic_Function/src/matemathic_function/main.py
@@ -6,33 +6,36 @@ if __name__ == "__main__":
     print(hello(friend))
     while True:
         triangle_type = input("Choose a type of triangle between equilateral, isosceles and scalene\n").lower()
-        if triangle_type == "equilateral":
-            side = float(input("Insert side length\n"))
-            if side < 0.0:
-                print("Sorry, invalid input")
+        try:
+            if triangle_type == "equilateral":
+                side = float(input("Insert side length\n"))
+                if side < 0.0:
+                    print("Sorry, invalid input")
+                else:
+                    et = EquilateralTriangle(side)
+                    print("Perimeter:", et.perimeter())
+                    print("Area:", et.area())
+                break
+            elif triangle_type == "isosceles":
+                base = float(input("Insert base side\n"))
+                lateral = float(input("Insert lateral side\n"))
+                if base < 0.0 or lateral < 0.0:
+                    print("Sorry, invalid input")
+                else:
+                    it = IsoscelesTriangle(base, lateral)
+                    print("Perimeter:", it.perimeter())
+                    print("Area:", it.area())
+                break
+            elif triangle_type == "scalene":
+                sides = [float(input("Insert side lenght\n")) for _ in range(3)]
+                if sides[0] < 0.0 or sides[1] < 0.0 or sides[2] < 0.0:
+                    print("Sorry, invalid input")
+                else:
+                    st = ScaleneTriangle(*sides)
+                    print("Perimeter:", st.perimeter())
+                    print("Area:", st.area())
+                break
             else:
-                et = EquilateralTriangle(side)
-                print("Perimeter:", et.perimeter())
-                print("Area:", et.area())
-            break
-        elif triangle_type == "isosceles":
-            base = float(input("Insert base side\n"))
-            lateral = float(input("Insert lateral side\n"))
-            if base < 0.0 or lateral < 0.0:
-                print("Sorry, invalid input")
-            else:
-                it = IsoscelesTriangle(base, lateral)
-                print("Perimeter:", it.perimeter())
-                print("Area:", it.area())
-            break
-        elif triangle_type == "scalene":
-            sides = [float(input("Insert side lenght\n")) for _ in range(3)]
-            if sides[0] < 0.0 or sides[1] < 0.0 or sides[2] < 0.0:
-                print("Sorry, invalid input")
-            else:
-                st = ScaleneTriangle(*sides)
-                print("Perimeter:", st.perimeter())
-                print("Area:", st.area())
-            break
-        else:
-            print("Sorry, I don't understand")
+                print("Sorry, I don't understand")
+        except ValueError:
+            print("Sorry, that was not a valid number. Please enter numeric values")
